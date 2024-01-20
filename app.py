0e7c8c41c0c14793118040fb02d235759dfb9010
MrCoder31 <147841674+MrCoder31@users.noreply.github.com>
2024-01-20 19:59:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ with col1:
   a=st.number_input('Enter a Value',value=10)
 with col2:
   b=st.text_input('Enter a text')
-  c=st.selectbox(label='choose',option=[1,2,3])
+  c=st.selectbox(label='choose',optionS=[1,2,3])
 sub=st.button(label='Submit')
 if sub:
   st.balloons()
